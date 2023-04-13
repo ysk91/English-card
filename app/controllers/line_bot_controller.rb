3d1a68ed13cf83268a1e92556b5d28674d2ab28b
ysk91 <ysk91.engineer@gmail.com>
2023-04-13 12:06:15 +0900

    シークレットキーの設定

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -2,4 +2,13 @@ class LineBotController < ApplicationController
   def callback
 
   end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
 end
